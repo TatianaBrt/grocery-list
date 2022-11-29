02697fd4d7929f9a6047046a991dae8af7628afe
TatianaBrt <brt79@mail.ru>
2022-11-29 22:28:29 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import image from './man.jpg';
+import image2 from './shop.jpg';
 import './App.css';
+import { GroceryList } from './GroceryList';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div className='container'>
+<img src={image2} alt="shop" width="200px"/> 
+</div>
+<h1>GROCERY LIST</h1>
+
+<GroceryList/>
+
+<div className='container'>
+<img src={image} alt="man" width="200px"/> 
+</div>      
     </div>
   );
 }
